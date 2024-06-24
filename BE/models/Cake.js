24af4ef66656c7f0549bb3fa42459ba56f031e9a
tdakhoa <tdakhoa@TDAKHOA>
2024-06-24 23:28:59 +0700

    add association for Cake and CakeImg

--- BE/models/Cake.js
+++ BE/models/Cake.js
@@ -44,54 +44,53 @@
 
 // module.exports = Cake;
 
-
-const { Model } = require('sequelize');
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-    class Cake extends Model {
-      static associate(models) {
-        // Cake.belongsToMany(models.Genre, { through: models.GenreCake, foreignKey: 'Cake' });
-        // Cake.hasMany(models.CakeTrailer, { foreignKey: 'Cake'})
-        // Cake.belongsToMany(models.Award, { through: models.AwardCake, foreignKey: 'Cake_id' });
-      }
+  class Cake extends Model {
+    static associate(models) {
+      Cake.hasMany(models.CakeImage, {
+        foreignKey: "cakeID",
+      });
     }
-  
-    Cake.init(
-      {
-        cakeID: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        priceCake:{
-            type: DataTypes.INTEGER,
-        },
-        purpose:{
-            type: DataTypes.STRING,
-            validated:{
-                len:[0, 100]
-            },
-        },
-        poster_path:{
-            type: DataTypes.STRING,
-            validated:{
-                len:[0, 255]
-            },
-        },
-        cakeFillingID:{
-            type: DataTypes.INTEGER,
-        },
-        cakeSizeID:{
-            type: DataTypes.INTEGER,
+  }
+
+  Cake.init(
+    {
+      cakeID: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      priceCake: {
+        type: DataTypes.INTEGER,
+      },
+      purpose: {
+        type: DataTypes.STRING,
+        validated: {
+          len: [0, 100],
         },
       },
-      {
-        sequelize,
-        modelName: 'Cake',
-        tableName: 'Cake',
-        timestamps: false
-      }
-    );
-  
-    return Cake;
-  };
+      poster_path: {
+        type: DataTypes.STRING,
+        validated: {
+          len: [0, 255],
+        },
+      },
+      cakeFillingID: {
+        type: DataTypes.INTEGER,
+      },
+      cakeSizeID: {
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Cake",
+      tableName: "Cake",
+      timestamps: false,
+    }
+  );
+
+  return Cake;
+};
