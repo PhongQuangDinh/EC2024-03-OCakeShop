c565becbec019f3bffe782e80c4b63bbf3881ede
uyeexnnhi <phamnhi936@gmail.com>
2024-08-03 13:40:23 +0700

    da dc Khoa fix cho

--- FE/ocake-shop/src/app/page.js
+++ FE/ocake-shop/src/app/page.js
@@ -1,5 +1,5 @@
-"use client"; 
-import { useState } from "react";
+
+
 import SignIn from "@/modules/SignIn/signin";
 import SignUp from "@/modules/SignUp/signup";
 import CartPage from "@/modules/Cart/cartpage";
@@ -13,8 +13,7 @@ export const metadata = {
 };
 
 export default function Home() {
-  const [showSignIn, setShowSignIn] = useState(false);
-  const [showSignUp, setShowSignUp] = useState(false);
+
 
   return (
     <div>
@@ -25,15 +24,7 @@ export default function Home() {
       <main>
         <AddCakePage />
         
-        <button onClick={() => setShowSignIn(!showSignIn)}>
-          {showSignIn ? "Hide Sign In" : "Show Sign In"}
-        </button>
-        <button onClick={() => setShowSignUp(!showSignUp)}>
-          {showSignUp ? "Hide Sign Up" : "Show Sign Up"}
-        </button>
-        
-        {showSignIn && <SignIn />}
-        {showSignUp && <SignUp />}
+     
       </main>
     </div>
   );
