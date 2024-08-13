1a984150efa13a3aa1f9fe32e7b02d5adc03d54a
uyeexnnhi <phamnhi936@gmail.com>
2024-08-13 14:02:22 +0700

    abc

--- BE/routes/UserRoute.js
+++ BE/routes/UserRoute.js
@@ -10,5 +10,12 @@ router.get('/', async (req, res, next) => {
     catch (err) {next(err)}
 });
 
+router.get('/:id', async (req, res, next) => {
+    try{
+        const user = await model.User.findByPk(req.params.id);
+        res.status(200).json(user);
+    }
+    catch (err) {next(err)}
+});
 
 module.exports = router;
