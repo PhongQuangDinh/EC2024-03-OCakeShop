ec71476e4879d51d859e59319c2bff9aea126b59
PhongGodFather <dinhquangphong365@gmail.com>
2024-08-26 02:46:01 +0700

    update BE to integrated with FE deployed app
    https://ec2024-03-ocakeshop.vercel.app
    Test this one

--- BE/index.js
+++ BE/index.js
@@ -14,8 +14,19 @@ app.use(morgan('combined'));
 
 app.use(express.json());
 
+// app.use(cors({
+//   origin: 'http://localhost:3000'
+// }));
+
+// Define allowed origins for CORS
+const allowedOrigins = [
+  'http://localhost:3000', // Local development
+  'https://ec2024-03-ocakeshop.vercel.app' // Deployed frontend on Vercel
+];
+
+// Use CORS middleware
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: allowedOrigins
 }));
 
 router(app);
