f90b031e269b80a1b2973f42721d802b9f315051
Hieu.dev <trunghieutran05042003@gmail.com>
2024-08-30 01:24:01 +0700

    increa, decrea, delete cake in cart

--- BE/routes/CartRoute.js
+++ BE/routes/CartRoute.js
@@ -3,58 +3,13 @@ const express = require('express');
 const router = express.Router();
 const model = require('../models');
 const {authenticateToken} = require('../routes/authenticationRoute');
-// const User = require('../models/User');
-// const Customer = require('../models/Customer');
-// const Cart = require('../models/Cart');
-
-// router.get('/cart/:id', async (req, res, next) => {
-//     try {
-//         const user = await model.User.findByPk(req.params.id, {
-//             include: [
-//                 {
-//                     model: Customer,
-//                     as: 'Customer',
-//                     include: [
-//                         {
-//                             model: Cart,
-//                             as: 'Cart',
-//                         }
-//                     ]
-//                 }
-//             ]
-//         });
-
-//         if (user) {
-//             res.status(200).json(user);
-//         } else {
-//             res.status(404).json({ message: "User not found" });
-//         }
-//     } catch (err) {
-//         next(err);
-//     }
-// });
-
-// module.exports = router;
-
-// router.get('/cart/:id', async (req, res, next) => {
-//     try{
-//         const user = await User.findByPk(req.params.id, {
-//             include: [
-//                 {
-//                     model: Customer,
-//                     as: 'Customer',
-//                     include: [
-//                         {
-//                             model: Cart,
-//                             as: 'Cart',
-//                         }
-//                     ]
-// const model = require('../models');
 
 router.get('/', authenticateToken, async (req, res, next) => {
     try{
         const userID = req.user.userID;
+        const role = req.user.role;
         const cart = await model.Cart.findAll({
+            where: {status: "Chưa mua"},
             include: [
                 {
                     model: model.Customer,
@@ -80,38 +35,6 @@ router.get('/', authenticateToken, async (req, res, next) => {
                     ]
                 }
             ]
-            // where: {userID: userID},
-            // include: [
-            //     {
-            //         model: model.Customer,
-            //         required: true,
-            //         include: [
-            //             {
-            //                 model: model.Cart,
-            //                 required: true,
-            //                 include: [
-            //                     {
-            //                       model: model.Cake,
-            //                       required: true,
-            //                       include: 
-            //                       [
-            //                         {
-            //                         model: model.CakeSize,
-            //                         as: "cakeSize",
-            //                         required: true,
-            //                         },
-            //                         {
-            //                           model: model.CakeFilling,
-            //                           as: "cakeFilling",
-            //                           required: true,
-            //                         }
-            //                       ]
-            //                     }
-            //                   ]
-            //             }
-            //         ]
-            //     }
-            // ]
         });
 
         if(!cart){
@@ -122,13 +45,6 @@ router.get('/', authenticateToken, async (req, res, next) => {
         else{
             return res.status(200).json(cart)
         }
-        // res.status(200).json(user);
-        // if(!cart){
-        //     return res.status(404).json({
-        //         message: "Cart is not exist"
-        //     });
-        // }
-        // res.status(200).json(cart);
     }
     catch (err) {next(err)};
 });
@@ -148,14 +64,29 @@ router.delete('/delete-cake/:id', authenticateToken, async (req, res, next) => {
     }
 });
 
-router.post('/update-cake/:id', async (req, res, next) => {
+router.post('/update-cake', async (req, res, next) => {
     try {
-        // const id = req.params.id;
-        // await model.Cart.destroy({
-        //     where: {cartID: id}
-        // });
-        // return res.status(200).json({ message: 'Item deleted successfully' });
-        
+        // const cartID = req.params.id;
+        const Cart = req.body;
+        // console.log(Cart);
+        const {cartID, quantity} = Cart;
+        console.log('quantity: ' + quantity);
+        console.log('cartID: ' + cartID);
+        const [cart] = await model.Cart.update(
+            {
+                quantity,
+            },
+            {
+                where: {cartID}
+            })
+        if(cart===0){
+            return res.status(404).json({ message: "Cart not found or no changes were made." });
+        }
+        const updatedCart = await model.Cart.findOne({
+            where: { cartID}
+        });
+
+        res.status(200).json(updatedCart);
     }
     catch(err){
         console.error('Error deleting item from cart:', err);
