1fdaa4b329031e6abffb0398208b35efd4baa3c6
tdakhoa <tdakhoa@TDAKHOA>
2024-08-31 15:16:01 +0700

    admin dashboard & header

--- BE/models/OrderCakeDetail.js
+++ BE/models/OrderCakeDetail.js
@@ -1,46 +1,55 @@
-const { Model } = require('sequelize');
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-    class OrderCakeDetail extends Model {
-      static associate(models) {
-        OrderCakeDetail.belongsTo(models.Cart, { foreignKey: 'cartID', as: 'OrderCart'});
-        OrderCakeDetail.belongsTo(models.OrderCake, { foreignKey: 'orderCakeID', as: 'Order'});
-      }
+  class OrderCakeDetail extends Model {
+    static associate(models) {
+      OrderCakeDetail.belongsTo(models.Cart, {
+        foreignKey: "cartID",
+        as: "OrderCart",
+      });
+      OrderCakeDetail.belongsTo(models.OrderCake, {
+        foreignKey: "orderCakeID",
+        as: "Order",
+      });
     }
-  
-    OrderCakeDetail.init(
-      {
-        orderCakeDetailID:{
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        bakingStatus:{
-            type: DataTypes.STRING,
-            validate: {
-                len:[0, 50],
-            },
-        },
-        handleStatus:{
-            type: DataTypes.STRING,
-            validate: {
-                len:[0, 50],
-            },
-        },
-        cartID:{
-            type: DataTypes.INTEGER,
-        },
-        orderCakeID:{
-            type: DataTypes.INTEGER,
-        }
+  }
+
+  OrderCakeDetail.init(
+    {
+      orderCakeDetailID: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
       },
-      {
-        sequelize,
-        modelName: 'OrderCakeDetail',
-        tableName: 'OrderCakeDetail',
-        timestamps: false
-      }
-    );
-  
-    return OrderCakeDetail;
-  };
+      arrange: {
+        type: DataTypes.INTEGER,
+      },
+      bakingStatus: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [0, 50],
+        },
+      },
+      handleStatus: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [0, 50],
+        },
+      },
+      cartID: {
+        type: DataTypes.INTEGER,
+      },
+      orderCakeID: {
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      sequelize,
+      modelName: "OrderCakeDetail",
+      tableName: "OrderCakeDetail",
+      timestamps: false,
+    }
+  );
+
+  return OrderCakeDetail;
+};
